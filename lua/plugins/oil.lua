f646bb3c3bd29a04b9da87ce4ddb9a91a2084d85
JudaNanaa <moussaadamou898@gmail.com>
2026-04-17 10:08:17 +0200

    add preview readme and in oil file navigator

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,21 @@ return {
 			keymaps = {
 				["q"] = "actions.close",
 			},
+			preview = {
+				max_width = 0.5,
+				min_width = 0.3,
+			},
+		})
+
+		-- Auto-preview au mouvement du curseur
+		vim.api.nvim_create_autocmd("CursorMoved", {
+			pattern = "oil://*",
+			callback = function()
+				local entry = require("oil").get_cursor_entry()
+				if entry then
+					require("oil.actions").preview.callback()
+				end
+			end,
 		})
 	end,
 }
