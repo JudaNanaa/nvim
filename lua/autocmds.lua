23bfaf78b37e31af492ae0e87d5b45325b29ce01
JudaNanaa <moussaadamou898@gmail.com>
2026-04-19 05:43:24 +0200

    add autosave

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,11 +1,23 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function()
-		vim.lsp.buf.format({ async = false })
+		local ft = vim.bo.filetype
+		if ft == "rust" or ft == "lua" or ft == "wgsl" then
+			vim.lsp.buf.format({ async = false })
+		end
 	end,
 })
 
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
 	callback = function()
 		vim.cmd("checktime")
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "WinLeave" }, {
+	pattern = "*",
+	callback = function()
+		if vim.bo.modified and vim.bo.buflisted and vim.fn.expand("%") ~= "" then
+			vim.cmd("silent! write")
+		end
 	end,
 })
