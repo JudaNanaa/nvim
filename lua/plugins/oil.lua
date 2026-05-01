ffbc79e9aa105b9cfeced324501878302bb1ef10
JudaNanaa <moussaadamou898@gmail.com>
2026-05-01 18:39:36 +0200

    change oil.lua

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,5 +1,5 @@
 return {
-	"stevearc/oil.nvim",
+	"barrettruth/canola.nvim",
 	dependencies = { "refractalize/oil-git-status.nvim" },
 	config = function()
 		require("oil").setup({
@@ -15,7 +15,22 @@ return {
 			},
 		})
 		require("oil-git-status").setup()
-		-- Auto-preview au mouvement du curseur
+
+		local function oil_resize()
+			local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+			local max_len = 0
+			for _, line in ipairs(lines) do
+				max_len = math.max(max_len, #line)
+			end
+			local width = math.max(20, math.min(max_len + 2, 50))
+			vim.api.nvim_win_set_width(0, width)
+		end
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+			pattern = "oil://*",
+			callback = oil_resize,
+		})
+
 		vim.api.nvim_create_autocmd("CursorMoved", {
 			pattern = "oil://*",
 			callback = function()
